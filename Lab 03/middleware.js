efaf50d68627577db7338f1661898492d2ddef84
MI-Mahin <mahmudulislammahin07@gmail.com>
2024-10-08 20:44:03 +0600

    Mistake

--- Lab 03/middleware.js
+++
@@ -1,9 +0,0 @@
-const ensureAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    next();
-  } else {
-    res.status(401).json({ error: "You do not have access" });
-  }
-};
-
-module.exports = ensureAuthenticated;
